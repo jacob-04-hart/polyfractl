db35dacef3beace2d6bc89bbe03e0746afe185cd
jacob-04-hart <jacob.04.hart@gmail.com>
2025-11-14 12:59:01 -0500

    removed cube comments

--- main.js
+++ main.js
@@ -29,11 +29,6 @@ loader.load( '/jc3.glb', function ( gltf ) {
 
 } );
 
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
 camera.position.z = 0;
 
 function animate() {
